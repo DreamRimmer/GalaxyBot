392b4b605a35c2eeb4fbfa9f01c32e97b66c41f8
DR-WP <143805920+DreamRimmer@users.noreply.github.com>
2024-11-13 14:57:34 +0530

    update, removed two tables

--- afc_reports.py
+++ afc_reports.py
@@ -11,10 +11,10 @@ pywikibot.config.user_agent = "en:User:GalaxyBot (dreamrimmer.wikimedian@gmail.c
 
 def query(interval):
     return f"""
-    SELECT 
-        RANK() OVER (ORDER BY COUNT(rc_id) DESC) AS Rank,
-        COUNT(rc_id) AS Reviews, 
-        actor_name AS Reviewer, 
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY COUNT(rc_id) DESC) AS Rank,
+        COUNT(rc_id) AS Reviews,
+        actor_name AS Reviewer,
         SUM(comment_text LIKE "%Publishing accepted%") AS "Accept",
         SUM(comment_text LIKE "Declining submission:%") AS "Decline",
         SUM(comment_text LIKE "Commenting on submission%") AS "Comment",
@@ -23,23 +23,23 @@ def query(interval):
         CONCAT(ROUND(SUM(comment_text LIKE "Declining submission:%") * 100 / COUNT(rc_id), 1), "%") AS "Decline %",
         CONCAT(ROUND(SUM(comment_text LIKE "Commenting on submission%") * 100 / COUNT(rc_id), 1), "%") AS "Comment %",
         CONCAT(ROUND(SUM(comment_text LIKE "Rejecting submission:%") * 100 / COUNT(rc_id), 1), "%") AS "Reject %"
-    FROM 
+    FROM
         recentchanges_userindex
-    LEFT JOIN 
+    LEFT JOIN
         actor ON rc_actor = actor_id
-    LEFT JOIN 
+    LEFT JOIN
         comment ON rc_comment_id = comment_id
-    WHERE 
-        (rc_namespace = 118 OR rc_namespace = 2) 
-        AND rc_type < 5 
-        AND (comment_text LIKE "Declining submission:%" 
+    WHERE
+        (rc_namespace = 118 OR rc_namespace = 2)
+        AND rc_type < 5
+        AND (comment_text LIKE "Declining submission:%"
              OR comment_text LIKE "Rejecting submission:%"
              OR comment_text LIKE "Commenting on submission%"
              OR comment_text LIKE "%Publishing accepted%")
         AND rc_timestamp >= NOW() - INTERVAL {interval}
-    GROUP BY 
+    GROUP BY
         rc_actor
-    ORDER BY 
+    ORDER BY
         Reviews DESC
     LIMIT 100;
     """
@@ -79,31 +79,28 @@ def main():
     intervals = {
         "Last 24 hours": "1 DAY",
         "Last 7 days": "7 DAY",
-        "Last 1 month": "1 MONTH",
-        "Last 3 months": "3 MONTH",
-        "Last 1 year": "1 YEAR"
+        "Last 1 month": "30 DAY"
     }
 
     now = datetime.now(timezone.utc)
     timestamp = now.strftime("%H:%M, %d %B %Y (UTC)")
     bot_username = pywikibot.Site().username()
-    content = f"""
-    <div style='font-size:24px'>Top AfC reviewers as of {timestamp}</div>
-    Updated by [[User:{bot_username}]] ([[User talk:{bot_username}|talk]]) <onlyinclude>{timestamp}</onlyinclude>
-    \n\n"""
+    content = "__TOC__\n\n"
+    content += f"<div style='font-size:24px'>Top AfC reviewers as of {timestamp}</div>\n"
+    content += f"Updated by [[User:{bot_username}]] ([[User talk:{bot_username}|talk]]) {timestamp}\n\n"
 
     for title, interval in intervals.items():
         rows, headers, time_taken = fetch(query(interval))
         content += f"== {title} ==\nQuery runtime: {time_taken:.2f} s\n"
         content += format_table(rows, headers) + "\n\n"
-    
+
     print(content)
 
     site = pywikibot.Site("en", "wikipedia")
     page = pywikibot.Page(site, "User:GalaxyBot/Reports/Top AfC reviewers")
 
-    page.text += "\n\n" + content
-    page.save("update AfC reports (bot)", bot=True)
+    page.text = content
+    page.save("update AfC reports (bot)", bot=True, minor=False)
 
 if __name__ == "__main__":
     main()
